3cb8930be2043a22bf18ce2cb7fde3a5c435980a
Timothyolo <97767706+Timothyolo@users.noreply.github.com>
2022-04-11 07:14:50 -0500

    added music to game, #594

---
+++ src/com/Imports/Music.java
@@ -0,0 +1,45 @@
+package com.Imports;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Music {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        Scanner scanner = new Scanner(System.in);
+        File file = new File("src/com/JsonObjects/group4.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File("src/com/JsonObjects/group4.wav"));
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        clip.start();
+
+        String response = scanner.next();
+
+
+
+
+       /*
+       String response ="";
+       while(!response.equals("Q"));
+        System.out.println("P = play, S = Stop");
+        System.out.println("enter your choice of option");
+
+        response = scanner.next();
+        response = response.toUpperCase();
+
+
+
+        switch (response){
+            case ("p"): clip.start();
+                break;
+            default: System.out.println("Not a valid response");
+        }  */
+
+
+
+
+
+    }
+}
